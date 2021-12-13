09a3f41a4d62276ff85fcd563ef512e884840799
McybearX <95560890+McybearX@users.noreply.github.com>
2021-12-13 13:33:26 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Mcrack
-Tools Hack Fb terbaru 2020 by MBEWLEGS
+# Command :
+      $ pkg update && upgrade
+      $ pkg install git
+      $ pkg install python2
+      $ pip2 install requests
+      $ pip2 install bs4
+      $ git clone https://github.com/McybearX/Mcrack
+      $ cd Mcrack
+      $ python2 Mcrack.py
---
+++ load.py
@@ -0,0 +1,320 @@
+#assalamu'alaikum warahmatullahi wabarakatuh🙏
+#jangan di recode plisss🥲
+
+import os,sys,time
+
+def aahh(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(1./10)
+
+def baner():
+    os.system("clear")
+    aahh("\n\x1b[1;97mLoading...")
+    print ("""\x1b[1;95m
+ʕ\x1b[1;91m X   x\x1b[1;95mʔ
+|   ─ ノ\x1b[1;97m
+|\,_  \ 
+| | | |
+ ￣  ￣
+""")
+
+def baner1():
+    print ("""
+Loading...
+\x1b[1;95m
+    ʕ \x1b[1;91mX   x\x1b[1;95mʔ
+    |   ─ ノ\x1b[1;97m
+    |\,_  \ 
+    | | | |
+     ￣  ￣
+""")
+
+def baner2():
+    os.system("clear")
+    print ("""
+Loading...
+\x1b[1;95m
+       ʕ \x1b[1;91mX   x\x1b[1;95mʔ
+       |   ─ ノ\x1b[1;97m
+       |\,_  \ 
+       | | | |
+        ￣  ￣
+""")
+
+def baner3():
+    os.system("clear")
+    print ("""
+Loading...
+\x1b[1;95m
+        ʕ \x1b[1;91mX   x\x1b[1;95mʔ
+        |   ─ ノ\x1b[1;97m
+        |\,_  \ 
+        | | | |
+         ￣  ￣
+""")
+
+def baner4():
+    os.system("clear")
+    print ("""
+Loading...
+\x1b[1;95m
+            ʕ \x1b[1;91mX   x\x1b[1;95mʔ
+            |   ─ ノ\x1b[1;97m
+            |\,_  \ 
+            | | | |
+             ￣  ￣
+""")
+
+def baner5():
+    os.system("clear")
+    print ("""
+Loading...
+\x1b[1;95m
+                ʕ\x1b[1;91m X   x\x1b[1;95mʔ
+                |   ─ ノ\x1b[1;97m
+                |\,_  \ 
+                | | | |
+                 ￣  ￣
+""")
+
+def baner6():
+    os.system("clear")
+    print ("""
+Loading...
+\x1b[1;95m
+                   ʕ \x1b[1;91mX   x\x1b[1;95mʔ
+                   |   ─ ノ\x1b[1;97m
+                   |\,_  \ 
+                   | | | |
+                    ￣  ￣
+""")
+
+def baner7():
+    os.system("clear")
+    print ("""
+Loading...
+\x1b[1;95m
+                     ʕ \x1b[1;91mX   x\x1b[1;95mʔ
+                     |   ─ ノ\x1b[1;97m
+                     |\,_  \ 
+                     | | | |
+                      ￣  ￣
+""")
+
+def baner8():
+    os.system("clear")
+    print ("""
+Loading...
+\x1b[1;95m
+                        ʕ\x1b[1;91m X   x\x1b[1;95mʔ
+                        |   ─ ノ\x1b[1;97m
+                        |\,_  \ 
+                        | | | |
+                         ￣  ￣
+""")
+
+def baner9():
+    os.system("clear")
+    print ("""
+Loading...
+\x1b[1;95m
+                          ʕ\x1b[1;91m X   x\x1b[1;95mʔ
+                          |   ─ ノ\x1b[1;97m
+                          |\,_  \ 
+                          | | | |
+                           ￣  ￣
+""")
+
+def baner9():
+    os.system("clear")
+    print ("""
+Loading...
+\x1b[1;95m
+                              ʕ\x1b[1;91m X   x\x1b[1;95mʔ
+                              |   ─ ノ\x1b[1;97m
+                              |\,_  \ 
+                              | | | |
+                               ￣  ￣
+""")
+
+def baner10():
+    os.system("clear")
+    print ("""
+Loading...
+\x1b[1;95m
+                                ʕ \x1b[1;91mX   x\x1b[1;95mʔ
+                                |   ─ ノ\x1b[1;97m
+                                |\,_  \ 
+                                | | | |
+                                 ￣  ￣
+""")
+
+def baner11():
+    os.system("clear")
+    print ("""
+Loading...
+\x1b[1;95m
+                                  ʕ \x1b[1;91mX   x\x1b[1;95mʔ
+                                  |   ─ ノ\x1b[1;97m
+                                  |\,_  \ 
+                                  | | | |
+                                   ￣  ￣
+""")
+
+def baner12():
+    os.system("clear")
+    print ("""
+Loading...
+\x1b[1;95m
+                                    ʕ\x1b[1;91m X   x\x1b[1;95mʔ
+                                    |   ─ ノ\x1b[1;97m
+                                    |\,_  \ 
+                                    | | | |
+                                     ￣  ￣
+""")
+
+def baner13():
+    os.system("clear")
+    print ("""
+Loading...
+\x1b[1;95m
+                                       ʕ\x1b[1;91m X   x\x1b[1;95mʔ
+                                       |   ─ ノ\x1b[1;97m
+                                       |\,_  \ 
+                                       | | | |
+                                        ￣  ￣
+""")
+
+def baner14():
+    os.system("clear")
+    print ("""
+Loading...
+\x1b[1;95m
+                                          ʕ\x1b[1;91m X   x\x1b[1;95mʔ
+                                          |   ─ ノ\x1b[1;97m
+                                          |\,_  \ 
+                                          | | | |
+                                           ￣  ￣
+""")
+
+def baner15():
+    os.system("clear")
+    print ("""
+Loading...
+\x1b[1;95m
+                                             ʕ\x1b[1;91m X   x\x1b[1;95mʔ
+                                             |   ─ ノ\x1b[1;97m
+                                             |\,_  \ 
+                                             | | | |
+                                              ￣  ￣
+""")
+
+def baner16():
+    os.system("clear")
+    print ("""
+Loading...
+\x1b[1;95m
+                                               ʕ\x1b[1;91m X   x\x1b[1;95mʔ
+                                               |   ─ ノ\x1b[1;97m
+                                               |\,_  \ 
+                                               | | | |
+                                                ￣  ￣
+""")
+
+def baner17():
+    os.system("clear")
+    print ("""
+Loading...
+\x1b[1;95m
+                                                ʕ\x1b[1;91m X   x\x1b[1;95mʔ
+                                                |   ─ ノ\x1b[1;97m
+                                                |\,_  \ 
+                                                | | | |
+                                                 ￣  ￣
+""")
+
+def baner18():
+    os.system("clear")
+    print ("""
+Loading...
+\x1b[1;95m
+                                                  ʕ\x1b[1;91m X   x\x1b[1;95m
+                                                  |   ─ ノ\x1b[1;97m
+                                                  |\,_  \ 
+                                                  | | | |
+                                                   ￣  ￣
+""")
+
+def baner19():
+    os.system("clear")
+    print ("""
+Loading...
+\x1b[1;95m
+                                                   ʕ\x1b[1;91m X   x\x1b[1;95mʔ
+                                                   |   ─ ノ\x1b[1;97m
+                                                   |\,_  \ 
+                                                   | | | |
+                                                    ￣  ￣
+""")
+
+def baner20():
+    os.system("clear")
+    print ("""
+Loading...
+\x1b[1;95m
+                                                      ʕ\x1b[1;91m X   x\x1b[1;95mʔ
+                                                      |   ─ ノ\x1b[1;97m
+                                                      |\,_  \ 
+                                                      | | | |
+                                                       ￣  ￣
+""")
+
+def loding():
+    baner()
+    time.sleep(3)
+    os.system("clear")
+    baner1()
+    time.sleep(1)
+    baner2()
+    time.sleep(1)
+    baner3()
+    time.sleep(1./5)
+    baner4()
+    time.sleep(1./5)
+    baner5()
+    time.sleep(1./10)
+    baner5()
+    time.sleep(1./10)
+    baner6()
+    time.sleep(1./10)
+    baner7()
+    time.sleep(1./10)
+    baner8()
+    time.sleep(1./5)
+    baner9()
+    time.sleep(1./5)
+    baner10()
+    time.sleep(1./5)
+    baner11()
+    time.sleep(1./5)
+    baner12()
+    time.sleep(1./5)
+    baner13()
+    time.sleep(1.5)
+    baner14()
+    time.sleep(1./7)
+    baner15()
+    time.sleep(1./7)
+    baner16()
+    time.sleep(1./8)
+    baner17()
+    time.sleep(1./10)
+    baner18()
+    time.sleep(1./6)
+    baner19()
+    time.sleep(1)
+    baner20()
+
+loding()
